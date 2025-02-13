a6b7d3a4973db87edd05e20633c9de1fa3bd0f6c
shafqat007 <shafqatnawazchy@gmail.com>
2025-02-13 11:14:30 +0600

    dd

--- index.js
+++ index.js
@@ -21,3 +21,5 @@ app.use("/api/tokens", require("./routes/tokens"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const authRouter = require("./routes/auth");
+app.use("/api/auth", authRouter);
